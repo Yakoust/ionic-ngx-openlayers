7d4b1e5528c497fb89c5d687ef5c33af5bea2f03
Marest <damien.marest@gmail.com>
2017-09-29 18:50:03 +0200

    poc ionic ngx-openlayers

---
+++ src/pages/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  public zoom = 15;
+  public opacity = 1.0;
+  public width = 5;
+
+}
